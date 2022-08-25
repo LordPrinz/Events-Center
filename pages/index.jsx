15ea028a5891eae3f875bdc47b5175250aee4fa2
LordPrinz <plazinski2902@gmail.com>
2022-08-25 23:18:37 +0200

    Add navigation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,16 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
+import MainHeader from "./../components/layout/main-header";
 
 function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+	const featuredEvents = getFeaturedEvents();
 
-  return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
-  );
+	return (
+		<div>
+			<MainHeader />
+			<EventList items={featuredEvents} />
+		</div>
+	);
 }
 
 export default HomePage;
