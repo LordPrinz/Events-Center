173df676d9670649b76c400a75dbc71b5e23c3a4
LordPrinz <plazinski2902@gmail.com>
2022-08-26 21:49:14 +0200

    Add _document.js

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
 			<Head>
+				<title>Next Events</title>
+				<meta name="description" content="Next Events" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
 
