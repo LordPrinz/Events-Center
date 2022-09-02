3f9585e1fccba9c2632e62a9a089fbf9fa8f9d97
LordPrinz <plazinski2902@gmail.com>
2022-09-02 22:29:33 +0200

    Add eventId route

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,35 @@
+export default handler = (req, res) => {
+    const eventId = req.query.eventId;
+
+    if (req.method === "POST") {
+        const { email, name, text } = req.body;
+        if (!email.includes("@") ||
+            !name ||
+            name.trim() === "" ||
+            !text ||
+            text.trim() === ""
+        ) {
+            res.status(422).json({ message: "Invalid input." });
+            return;
+        }
+
+        const newComment = {
+            id: new Date().toISOString(),
+            email,
+            name,
+            text,
+        };
+
+        console.log(newComment);
+
+        res.status(201).json({ message: "Added comment.", comment: newComment });
+    }
+
+    if (req.method === "GET") {
+        const dummyList = [
+            { id: "c1", name: "Oskar", text: "A first comment!" },
+            { id: "c2", name: "Paweł", text: "A second comment!" },
+        ];
+        res.status(200).json({ comments: dummyList });
+    }
+};
