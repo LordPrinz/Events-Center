12de941ebf3bbae3be997315ac3e03eef4228fca
LordPrinz <plazinski2902@gmail.com>
2022-09-03 23:32:29 +0200

    Showing notifications on registration feature

--- components/input/newsletter-registration.jsx
+++ components/input/newsletter-registration.jsx
@@ -1,14 +1,22 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
+import NotificationContext from "../../store/notification-context";
 
 import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
 	const emailInputRef = useRef();
+	const notificationCtx = useContext(NotificationContext);
 
 	function registrationHandler(event) {
 		event.preventDefault();
 
 		const enteredEmail = emailInputRef.current.value;
+
+		notificationCtx.showNotification({
+			title: "Signing up...",
+			message: "Registering for newsletter.",
+			status: "pending",
+		});
 
 		fetch("/api/newsletter", {
 			method: "POST",
@@ -16,7 +24,30 @@ function NewsletterRegistration() {
 			headers: {
 				"Content-Type": "application/json",
 			},
-		}).then((response) => response.json());
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+
+				response.json().then((data) => {
+					throw new Error(data.message || "Something went wrong!");
+				});
+			})
+			.then((data) => {
+				notificationCtx.showNotification({
+					title: "Success!",
+					messaeg: "Successfully registered for newsletter",
+					status: "success",
+				});
+			})
+			.catch((error) => {
+				notificationCtx.showNotification({
+					title: "Error!",
+					messaeg: error.message || "Something went wrong!",
+					status: "error",
+				});
+			});
 	}
 
 	return (
